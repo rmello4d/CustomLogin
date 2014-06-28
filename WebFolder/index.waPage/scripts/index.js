6f0a0927d39bc1bf2344da4dab9472da6f134c7a
Ricardo Mello <ricardo.mello@4d.com>
2014-06-28 14:12:51 +0200

    Improvement to help testing and coding
    This modification was made to avoid log-in everytime you refresh the page => practical when coding and testing

--- WebFolder/index.waPage/scripts/index.js
+++ WebFolder/index.waPage/scripts/index.js
@@ -9,7 +9,13 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
-		WakTmpl.init();
+		 if (WAF.directory.currentUser() && $("#waf-body .loginButton").text() != 'Log Out'){
+			$$('mainComponent').loadComponent({path: '/app.waComponent'});
+			$$('switchStateButton').setValue('Log Out');
+			$$('loginText').setValue("Logged in as: " + waf.directory.currentUser().fullName);
+		} else {
+			WakTmpl.init();
+		}
 	};// @lock
 
 // @region eventManager// @startlock
